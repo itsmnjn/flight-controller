ce1794911796cd98b848505688b39795d18f81e2
Min Jun Kim <mk7pe@virginia.edu>
2020-05-23 17:07:33 -0400

    Comment out yet-to-work I2C code

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
+#include "bmi088.h"
+#include "bmi08x.h"
 #include "motors.h"
 
 int main(void) {
   initMotors();
+
+  // int8_t rslt;
+
+  // struct bmi08x_dev dev = {.accel_id = BMI08X_ACCEL_I2C_ADDR_PRIMARY,
+  //                          .gyro_id = BMI08X_GYRO_I2C_ADDR_PRIMARY,
+  //                          .intf = BMI08X_I2C_INTF,
+  //                          .read = (void *)0,
+  //                          .write = (void *)0,
+  //                          .delay_ms = (void *)0};
+
+  /* Initialize the I2C */
+
+  /* Initializing the bmi085 sensors the below function will Initialize both
+   * accel and gyro sensors */
+  // rslt = bmi088_init(&dev);
+
   while (1) {
     // testMotors();
   }
